4190aaaeb8c61dc805a56a8de1a2405f7b29ad11
lera <klementievavaleria@gmail.com>
2024-04-24 03:15:08 +0300

    rewiew-4

---
+++ Task4/cmd/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	//"fmt"
+	"fmt"
+	"os"
+	"task4/comic"
+
+	//"time"
+
+	"gopkg.in/yaml.v2"
+)
+
+func main() {
+
+	//comic.MakeJSONWithComicsData(readConfig())
+
+	// выбрать один из вариантов
+	fmt.Println(comic.SimpleSearch(comic.InputProccessing()))
+	fmt.Println(comic.IndexSearch(comic.InputProccessing()))
+
+}
+
+// Возвращает переменные из "config.yaml"
+func readConfig() (string, string, int) {
+	// Чтение файла
+
+	configName := comic.ParsArgument_C()
+
+	content, err := os.ReadFile(configName)
+	if err != nil {
+		panic(err)
+	}
+
+	// Разбор YAML-контента
+	var yamlMap map[string]interface{}
+	err = yaml.Unmarshal(content, &yamlMap)
+	if err != nil {
+		panic(err)
+	}
+
+	source_url, ok := yamlMap["source_url"].(string)
+	if !ok {
+		panic("Неверный тип данных для 'source_url'")
+	}
+	db_file, ok := yamlMap["db_file"].(string)
+	if !ok {
+		panic("Неверный тип данных для 'db_file'")
+	}
+	parallel, ok := yamlMap["parallel"].(int)
+	if !ok {
+		panic("Неверный тип данных для 'parallel'")
+	}
+
+	return source_url, db_file, parallel
+}
